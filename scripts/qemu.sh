63913486290257a9ef943ab90a14fa719819f5aa
Gwins7 <gwins777@qq.com>
2024-04-23 10:17:40 +0800

    update scripts

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -1,7 +1,7 @@
 qemu-system-riscv64 -M virt -m 256M -nographic \
-	-bios opensbi/build/platform/generic/firmware/fw_jump.bin \
-	-kernel riscv-linux/arch/riscv/boot/Image \
+	-bios ../opensbi/build/platform/generic/firmware/fw_jump.bin \
+	-kernel ../riscv-linux/arch/riscv/boot/Image \
 	-append "console=ttyS0 rw root=/dev/vda" \
-	-drive file=rootfs.img,format=raw,id=hd0 \
+	-drive file=../rootfs.img,format=raw,id=hd0 \
 	-device virtio-blk-device,drive=hd0 \
 	
