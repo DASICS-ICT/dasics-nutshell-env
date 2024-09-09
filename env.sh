d6d7ac1db513e73b8931e397263f555f1be90ed6
Gwins7 <Gwins777@qq.com>
2024-09-09 15:44:53 +0800

    env: update sh

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 #dasics-nutshell
-export DASICS_NUTSHELL_HOME=$(abspath .)
+export DASICS_NUTSHELL_HOME=$(pwd)
 export NOOP_HOME=$DASICS_NUTSHELL_HOME/NutShell-DASICS
 export NEMU_HOME=$DASICS_NUTSHELL_HOME/NEMU
 export RISCV_ROOTFS_HOME=$DASICS_NUTSHELL_HOME/riscv-rootfs
 export RISCV_QEMU=$DASICS_NUTSHELL_HOME/qemu-8.1.0/build
 
 #toolchain
-export DASICS_TOOL_CHAIN=$(abspath .)/riscv64-toolchain
+export DASICS_TOOL_CHAIN=$(pwd)/riscv64-toolchain
 export RISCV=$DASICS_TOOL_CHAIN
 #export PATH=$DASICS_TOOL_CHAIN/riscv64-unknown-elf/bin:$PATH
 export PATH=$DASICS_TOOL_CHAIN/bin:$RISCV_QEMU:$PATH
